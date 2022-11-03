2fa89942612cfdea0857bc56173552355ae9eeb3
Aliaksei Piantsiak <avpentyak@gmail.com>
2022-11-03 16:48:35 +0100

    Refactor code using pre-commit features

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, re_path, include
-
+from django.urls import include, path, re_path
 
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path("auth/", include("djoser.urls")),
+    re_path(r"^auth/", include("djoser.urls.authtoken")),
 ]
